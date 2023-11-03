7478ebc113b7914dca9c9b1c754f190254c2ac82
DudleyBear <149424104+DudleyBear@users.noreply.github.com>
2023-11-03 15:22:03 -0400

    Create Circle.js

---
+++ Circle.js
@@ -0,0 +1,15 @@
+let colorlist = ['gold', 'yellow', 'turquoise', 'red']
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(52, 229, 235);
+  //rectMode(CENTER);
+  //rect(width/2,height/2,125,100)
+  ellipse(400, 400, 300, 300);
+}
+
+//function draw() {
+  //noStroke()
+  //fill(random(colorlist));
+  //ellipse(mouseX, mouseY, 25, 25);
+//}
